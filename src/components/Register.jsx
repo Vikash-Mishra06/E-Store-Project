69f98670522476316706271576ffb67149a52292
Vikash Mishra <mishravikash0612@gmail.com>
2025-09-08 23:17:24 +0530

    twelveth commit

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -8,6 +8,14 @@ const Register = () => {
       <div className="w-full md:w-1/2 lg:w-1/3 shadow-lg rounded-lg p-8 border border-gray-400">
         <h2 className="text-2xl text-red-500 font-bold mb-6 text-center">Register</h2>
         <form>
+            <div className="mb-4">
+            <label className="block text-gray-700 mb-1">Username</label>
+            <input
+              className="w-full px-3 py-2 border border-gray-400 rounded"
+              type="text"
+              placeholder="Enter Name"
+            />
+          </div>
           <div className="mb-4">
             <label className="block text-gray-700 mb-1">Email</label>
             <input
